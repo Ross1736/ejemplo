a4aa66829392c036755a0a26a9edf812ec463a1e
Ross1736 <bryan1736b@gmail.com>
2022-08-09 20:11:22 -0500

    primer ejercicio

---
+++ js/1_main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 500,
+  },
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
